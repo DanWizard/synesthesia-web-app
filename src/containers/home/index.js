0466de1393955545906e7790b4b67cc2d74932d3
Daniel Warrick <42007188+DanWizard@users.noreply.github.com>
2020-07-25 21:25:23 -0400

    current code

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -12,13 +12,23 @@ import { hertz } from "./notes";
 
 const Home = () => {
   const [start, setStart] = useState(null);
+  const [startAnalyzer, setStartAnalyzer] = useState(null);
+  const [swi, setSwitch] = useState(null);
   const [clicked, setC] = useState(null);
   const [freq, setFreq] = useState(null);
   const [timeFunc, setFunc] = useState(null);
   const freqBinLen = 256;
-  const f_sample = hertz["B8"] * 2;
+  const f_sample = hertz["B6"] * 2;
   const period = (1 / f_sample) * 1000;
   const radialGCC = 12;
+  // console.log(period);
+  // console.log(f_sample);
+  // console.log(f_sample);
+  // console.log(f_sample);
+  // console.log(f_sample);
+  // console.log(f_sample);
+  // console.log(f_sample);
+  // const cutOffs = []
 
   // console.log(rate);
   // console.log(time);
@@ -32,12 +42,26 @@ const Home = () => {
   };
 
   const listen = async () => {
-    const si = Sound
-    const sp = await si.input()
-    debugger
-    const s = await Sound.input();
-    const activeSound = s.analyze(freqBinLen);
+    // const si = Sound;
+    // const sp = await si.input();
+    // debugger;
+    // const s = await Sound.input();
+    // const activeSound = s.analyze(freqBinLen, -65);
+    const activeSound = new window.AudioContext({ sampleRate: f_sample });
+    const analyzer = activeSound.createAnalyser();
+    analyzer.minDecibals = -100;
+    analyzer.maxDecibals = -30;
+    analyzer.smoothingTimeConstant = 0.8;
+    analyzer.fftSize = 256;
+    const bufferLength = analyzer.frequencyBinCount;
+    const dataArray = new Uint8Array(bufferLength);
+    console.log(analyzer.getByteFrequencyData(dataArray));
+    console.log(activeSound);
+    console.log(analyzer);
+
+    if (activeSound) debugger;
     setStart(activeSound);
+    setStartAnalyzer(analyzer);
     change();
   };
 
@@ -49,7 +73,7 @@ const Home = () => {
         change();
       }
     }
-  }, [start, clicked]);
+  }, [start, clicked, startAnalyzer, swi]);
 
   const getMean = (arr) => {
     let sum = 0;
@@ -59,14 +83,28 @@ const Home = () => {
     return sum / arr.length;
   };
 
-  const getMax = (arr) => { //need to return max magnitude AND index of frequency bin
-    return Math.max(...arr);
+  const getMax = (arr) => {
+    //need to return max magnitude AND index of frequency bin
+    let max = 0;
+    let index = null;
+    // [1,2,3,4]
+    arr.forEach((element, i) => {
+      if (element > max) {
+        max = element;
+        index = i;
+      }
+    });
+    return { max, index };
   };
 
   const change = () => {
     if (!timeFunc) {
       const func = setInterval(() => {
-        if (start) {
+        console.log("does not exist");
+        debugger;
+        if (start || startAnalyzer) {
+          console.log("does exist");
+          debugger;
           const f = filterFreq();
           // console.log(f);
           setFreq(f);
@@ -81,9 +119,12 @@ const Home = () => {
   };
 
   const filterFreq = () => {
+    console.log("does not exist");
+    debugger;
     if (start) {
+      console.log("does exist");
+      debugger;
       let arr = start.freqDomain();
-      // arr = arr.slice(0, 250);
       let iter = 0;
       let count = 0;
       const fSet = Math.floor(arr.length / radialGCC);
@@ -95,24 +136,32 @@ const Home = () => {
         if (iter === 0) {
           add = remainder;
         }
-        buckets[iter] = getMax(arr.slice(count, count + fSet + add));
+
+        const vals = getMax(arr.slice(count, count + fSet + add));
+        const IndexOfMaxY = vals.index;
+        buckets[iter] = vals.max;
         count = count + fSet + add;
 
-        const delta = 0.5 * ((arr[IndexOfMaxY-1] - arr[IndexOfMaxY+1]) / (arr[IndexOfMaxY-1] - (2.0 * arr[IndexOfMaxY]) + arr[IndexOfMaxY+1]));
-        const interpolatedX = ((IndexOfMaxY + delta)  * f_sample) / (freqBinLen-1);
-        if(IndexOfMaxY==(Math.floor(freqBinLin/2))){ //To improve calculation on edge values
-          interpolatedX = ((IndexOfMaxY + delta)  * f_sample) / (freqBinLen);
+        const delta =
+          0.5 *
+          ((arr[IndexOfMaxY - 1] - arr[IndexOfMaxY + 1]) /
+            (arr[IndexOfMaxY - 1] -
+              2.0 * arr[IndexOfMaxY] +
+              arr[IndexOfMaxY + 1]));
+        const interpolatedX =
+          ((IndexOfMaxY + delta) * f_sample) / (freqBinLen - 1);
+        if (IndexOfMaxY == Math.floor(freqBinLen / 2)) {
+          //To improve calculation on edge values
+          interpolatedX = ((IndexOfMaxY + delta) * f_sample) / freqBinLen;
         }
         iter += 1;
       } while (iter < radialGCC);
-      console.log(testFobj);
+      // console.log(testFobj);
       // console.log(fObj);
       return buckets;
     } else {
       return 0;
     }
-
-
   };
 
   // const g = () => {
